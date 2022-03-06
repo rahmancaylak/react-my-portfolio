a1d83300e4f9a0b2ca24d808658e89f58e5f4a56
Rahman Çaylak <rahmancaylakk@gmail.com>
2022-03-06 19:01:34 +0300

    hero component created

---
+++ src/components/Hero/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Photo from '../../assets/Img/rahmancaylak.jpg';
+// Fontawesome Icons
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+  FaAngleDown,
+} from 'react-icons/fa';
+// Tailwind Styled Components
+import {
+  SectionHero,
+  HeroContainer,
+  HeroSidebar,
+  HeroContent,
+  Ul,
+  Li,
+  Container,
+  Img,
+  H1,
+  AboutArrow,
+} from '../../styles/Hero';
+function Hero() {
+  return (
+    <SectionHero>
+      <HeroContainer>
+        <HeroSidebar>
+          <Ul>
+            <Li>
+              <a href='https://github.com/rahmancaylak'>
+                <FaGithub className='w-6 h-6 hover:opacity-70 inline' />
+              </a>
+            </Li>
+            <Li>
+              <a href='https://www.linkedin.com/in/rahman-caylak/'>
+                <FaLinkedin className='w-6 h-6 hover:text-sky-700 inline' />
+              </a>
+            </Li>
+            <Li>
+              <a href='https://twitter.com/rahmancaylak'>
+                <FaTwitter className='w-6 h-6 hover:text-sky-500 inline' />
+              </a>
+            </Li>
+            <Li>
+              <a href='https://instagram.com/rahmancaylak'>
+                <FaInstagram className='w-6 h-6 inline hover:text-purple-500' />
+              </a>
+            </Li>
+          </Ul>
+        </HeroSidebar>
+        <HeroContent>
+          <Container>
+            <Img
+              src={`${Photo}`}
+              alt="Rahman Çaylak'ın beyaz bir arkaplanda yüzü açık bir şekilde çekilmiş fotoğrafı."
+            ></Img>
+            <H1>Rahman Çaylak</H1>
+            <AboutArrow href='#about'>
+              <FaAngleDown className='w-8 h-8' />
+            </AboutArrow>
+          </Container>
+        </HeroContent>
+      </HeroContainer>
+    </SectionHero>
+  );
+}
+
+export default Hero;
---
+++ src/styles/Hero/index.js
@@ -0,0 +1,71 @@
+import tw from 'tailwind-styled-components';
+
+export const SectionHero = tw.div`
+dark
+mx-auto 
+grid 
+h-screen 
+text-white 
+bg-customPrimary
+`;
+
+export const HeroContainer = tw.div`
+grid 
+grid-cols-3 
+gap-4
+`;
+
+export const HeroSidebar = tw.div`
+px-5
+py-6
+mt-10
+`;
+
+export const Ul = tw.ul`
+`;
+
+export const Li = tw.li`
+py-5
+`;
+
+export const HeroContent = tw.div`
+justify-center
+flex 
+`;
+
+export const Container = tw.div`
+flex
+flex-col
+items-center
+my-16
+`;
+
+export const Img = tw.img`
+rounded-full
+md:h-64
+md:w-64
+sm:h-40
+sm:w-40
+`;
+
+export const H1 = tw.h1`
+text-center
+text-4xl
+uppercase
+font-medium
+mt-5
+`;
+
+export const AboutArrow = tw.a`
+mb-auto
+mt-16
+animate-bounce 
+p-2 
+w-10 
+h-10 
+shadow-lg 
+rounded-full 
+flex 
+items-center 
+justify-center
+`;
