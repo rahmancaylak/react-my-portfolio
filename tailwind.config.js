1d65acb0616d683e7e89767f3ba98e5d53cd7e56
Rahman Çaylak <rahmancaylakk@gmail.com>
2022-04-07 01:58:23 +0300

    Tailwind config updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,15 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    screens: {
-      sm: [{ min: '100px', max: '805px' }],
-      md: [
-        // Sidebar appears at 768px, so revert to `sm:` styles between 768px
-        // and 868px, after which the main content area is wide enough again to
-        // apply the `md:` styles.
-        { min: '806px', max: '1098px' },
-        { min: '868px' },
-      ],
-      lg: '1100px',
-      xl: '1400px',
-    },
     extend: {
       colors: {
-        customPrimary: '#1d1e24',
-        customSecondary: '#23262b',
-        customBorderColor: '#33363D',
-        customInputColor: '#2E2F36',
+        rcPrimary: '#1d1e24',
+        rcSecondary: '#23262b',
+        rcBorder: '#33363D',
+        rcInput: '#2E2F36',
+        rcLightInput: '#d9d8d8',
+        rcLightPrimary: '#79787882',
+        rcLightSecondary: '#C2C2C2',
       },
     },
   },
